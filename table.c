2f1333a657fcd6f885d5a20cd51d5d686b913593
ediraphael <raphael.pillie@gmail.com>
2014-04-08 18:29:25 +0200

    Ajout de fonctions dans table.c

--- table.c
+++ table.c
@@ -8,3 +8,48 @@ struct element
 };
  
 typedef element* llist;
+
+llist ajouterEnTete(llist liste, int valeur)
+{
+    /* On crée un nouvel élément */
+    element* nouvelElement = malloc(sizeof(element));
+ 
+    /* On assigne la valeur au nouvel élément */
+    nouvelElement->val = valeur;
+ 
+    /* On assigne l'adresse de l'élément suivant au nouvel élément */
+    nouvelElement->nxt = liste;
+ 
+    /* On retourne la nouvelle liste, i.e. le pointeur sur le premier élément */
+    return nouvelElement;
+}
+
+llist ajouterEnFin(llist liste, int valeur)
+{
+    /* On crée un nouvel élément */
+    element* nouvelElement = malloc(sizeof(element));
+ 
+    /* On assigne la valeur au nouvel élément */
+    nouvelElement->val = valeur;
+ 
+    /* On ajoute en fin, donc aucun élément ne va suivre */
+    nouvelElement->nxt = NULL;
+ 
+    if(liste == NULL)
+    {
+        /* Si la liste est videé il suffit de renvoyer l'élément créé */
+        return nouvelElement;
+    }
+    else
+    {
+        /* Sinon, on parcourt la liste à l'aide d'un pointeur temporaire et on
+        indique que le dernier élément de la liste est relié au nouvel élément */
+        element* temp=liste;
+        while(temp->nxt != NULL)
+        {
+            temp = temp->nxt;
+        }
+        temp->nxt = nouvelElement;
+        return liste;
+    }
+}
